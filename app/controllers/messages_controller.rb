7c677ac19fa7082fc2ff252e835ca380c2e69502
rafuto <ganpurafuto@ezweb.ne.jp>
2016-08-01 12:52:27 +0000

    add some implementations.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def index
     @message = Message.new
      # Messageを全て取得する。
     @messages = Message.all
-  end
-
-  def create
-    @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
   end
   
   def create
@@ -22,10 +31,19 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
   ## ここまで
 end
